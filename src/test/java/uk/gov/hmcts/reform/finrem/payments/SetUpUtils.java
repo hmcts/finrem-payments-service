9ce9c0a1152063665bd165d7dd482e29f0b71a93
shaibazkhan <sknizami@gmail.com>
2019-01-17 17:37:48 +0000

    added e2e test

--- src/test/java/uk/gov/hmcts/reform/finrem/payments/SetUpUtils.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/payments/SetUpUtils.java
@@ -7,12 +7,16 @@ import com.google.common.collect.ImmutableMap;
 import feign.FeignException;
 import feign.Response;
 import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.reform.finrem.payments.model.fee.Fee;
 import uk.gov.hmcts.reform.finrem.payments.model.pba.payment.FeeRequest;
 import uk.gov.hmcts.reform.finrem.payments.model.pba.payment.PaymentRequest;
 import uk.gov.hmcts.reform.finrem.payments.model.pba.payment.PaymentResponse;
 import uk.gov.hmcts.reform.finrem.payments.model.pba.payment.PaymentStatusHistory;
+import uk.gov.hmcts.reform.finrem.payments.model.pba.validation.PBAAccount;
 
+import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.List;
 
 public class SetUpUtils {
 
@@ -21,11 +25,39 @@ public class SetUpUtils {
     public static final String PAYMENT_STATUS = "Success";
     public static final String PAYMENT_REF = "RC-12345-2323-0712321320-23221";
 
+    public static final String FEE_CODE = "CODE";
+    public static final String FEE_DESC = "Description";
+    public static final BigDecimal FEE_AMOUNT = new BigDecimal("10");
+    public static final String FEE_VERSION = "v1";
+
+    public static final String PBA_NUMBER = "PBA";
+
     private static ObjectMapper objectMapper = new ObjectMapper();
 
     public static FeignException feignError() {
         Response response = Response.builder().status(STATUS_CODE).headers(ImmutableMap.of()).build();
         return FeignException.errorStatus("test", response);
+    }
+
+    public static String pBAAccount() {
+        PBAAccount pbaAccount = new PBAAccount();
+        pbaAccount.setAccountList(ImmutableList.of(PBA_NUMBER));
+
+        return objectToJson(pbaAccount);
+    }
+
+    public static Fee feeResponse() {
+        Fee fee = new Fee();
+        fee.setCode(FEE_CODE);
+        fee.setDescription(FEE_DESC);
+        fee.setFeeAmount(FEE_AMOUNT);
+        fee.setVersion(FEE_VERSION);
+
+        return fee;
+    }
+
+    public static String feeResponseString() {
+        return objectToJson(feeResponse());
     }
 
     public static PaymentResponse paymentResponse() {
@@ -51,11 +83,7 @@ public class SetUpUtils {
     }
 
     public static String paymentRequestStringContent() {
-        try {
-            return objectMapper.writeValueAsString(paymentRequest());
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return objectToJson(paymentRequest());
     }
 
     public static PaymentRequest paymentRequest() {
@@ -76,14 +104,18 @@ public class SetUpUtils {
     }
 
     private static String paymentResponseToString(PaymentResponse paymentResponse) {
-        try {
-            return objectMapper.writeValueAsString(paymentResponse);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return objectToJson(paymentResponse);
     }
 
     private static PaymentStatusHistory paymentStatusHistory() {
         return PaymentStatusHistory.builder().errorCode("ERR").errorMessage("error").status("S").build();
+    }
+
+    private static String objectToJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
