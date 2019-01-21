a182a343d4589298179d1d1f642380775b765d2c
shaibazkhan <sknizami@gmail.com>
2019-01-21 16:46:00 +0000

    removed unused imports

--- src/main/java/uk/gov/hmcts/reform/finrem/payments/health/IdamServiceHealthCheck.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/payments/health/IdamServiceHealthCheck.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.finrem.payments.health;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
