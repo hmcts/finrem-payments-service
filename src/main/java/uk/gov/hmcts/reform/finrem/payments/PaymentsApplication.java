f5cb0079efa983db3d3135fbdb2a9793dd973895
shaibazkhan <sknizami@gmail.com>
2019-01-24 10:19:37 +0000

    skip ssl cert check

--- src/main/java/uk/gov/hmcts/reform/finrem/payments/PaymentsApplication.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/payments/PaymentsApplication.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.reform.finrem.payments;
 
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.TrustStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,12 +10,6 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import uk.gov.hmcts.reform.authorisation.healthcheck.ServiceAuthHealthIndicator;
-
-import javax.net.ssl.SSLContext;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
 
 @SpringBootApplication(exclude = {ServiceAuthHealthIndicator.class})
 public class PaymentsApplication {
@@ -37,15 +29,17 @@ public class PaymentsApplication {
         return loggingFilter;
     }
 
-    public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
-        TrustStrategy acceptingTrustStrategy = (X509Certificate[] x509Certificates, String value) -> true;
+    @Bean
+    public RestTemplate restTemplate() {
 
-        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy).build();
-        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
-        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        CloseableHttpClient httpClient
+                = HttpClients.custom()
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
+        HttpComponentsClientHttpRequestFactory requestFactory
+                = new HttpComponentsClientHttpRequestFactory();
         requestFactory.setHttpClient(httpClient);
+
         return new RestTemplate(requestFactory);
     }
 }
