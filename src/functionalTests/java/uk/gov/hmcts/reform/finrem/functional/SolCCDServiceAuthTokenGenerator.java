0514bbf64c35ccf4d1ffd7ad1f1c45ab8a91501a
shaibazkhan <sknizami@gmail.com>
2019-02-15 21:01:37 +0000

    some modificationto s2s auth token generation

--- src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/SolCCDServiceAuthTokenGenerator.java
+++ src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/SolCCDServiceAuthTokenGenerator.java
@@ -2,13 +2,13 @@ package uk.gov.hmcts.reform.finrem.functional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
 @Component
 public class SolCCDServiceAuthTokenGenerator {
 
     @Autowired
-    private ServiceAuthTokenGenerator tokenGenerator;
+    private AuthTokenGenerator tokenGenerator;
 
     public String generateServiceToken() {
         return tokenGenerator.generate();
--- src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/TestContextConfiguration.java
+++ src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/TestContextConfiguration.java
@@ -1,36 +1,34 @@
 package uk.gov.hmcts.reform.finrem.functional;
 
 
-import feign.Feign;
-import feign.jackson.JacksonEncoder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
-import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 
 @Slf4j
 @Configuration
 @ComponentScan("uk.gov.hmcts.reform.finrem.functional")
-@EnableFeignClients(basePackageClasses = ServiceAuthorisationApi.class)
 @PropertySource("classpath:application.properties")
 @PropertySource("classpath:application-aat.properties")
+@EnableFeignClients(basePackageClasses = ServiceAuthorisationApi.class)
 public class TestContextConfiguration {
 
     @Bean
-    public ServiceAuthTokenGenerator serviceAuthTokenGenerator(@Value("${idam.auth.secret}")
+    public AuthTokenGenerator serviceAuthTokenGenerator(@Value("${idam.auth.secret}")
                                                                    String secret,
-                                                               @Value("${idam.auth.microservice}")
-                                                                       String microservice,
-                                                               final ServiceAuthorisationApi serviceAuthorisationApi) {
-        log.info("service.name: {}", microservice);
+                                                        @Value("${idam.auth.microservice}")
+                                                                       String microService,
+                                                        final ServiceAuthorisationApi serviceAuthorisationApi) {
+        log.info("service.name: {}", microService);
         log.info(": {idam.s2s-auth.secret}", secret);
-        return new ServiceAuthTokenGenerator(secret, microservice, serviceAuthorisationApi);
+        return AuthTokenGeneratorFactory.createDefaultGenerator(secret, microService, serviceAuthorisationApi);
     }
 }
