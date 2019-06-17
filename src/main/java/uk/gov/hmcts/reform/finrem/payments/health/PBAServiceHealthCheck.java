619b9d67be6d243b8f5b390fc98a5b6e0aa8c1b1
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2019-06-17 13:22:38 +0100

    FR_1066_Integration_with_new_PRD_API
    - updated health check
    - updated unit tests

--- src/main/java/uk/gov/hmcts/reform/finrem/payments/health/PBAServiceHealthCheck.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/payments/health/PBAServiceHealthCheck.java
@@ -1,13 +1,73 @@
 package uk.gov.hmcts.reform.finrem.payments.health;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
-@Component
-public class PBAServiceHealthCheck extends AbstractServiceHealthCheck {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-    public PBAServiceHealthCheck(@Value("${pba.validation.health.url}") String uri, RestTemplate restTemplate) {
-        super(uri, restTemplate);
+@Slf4j
+@Component
+public class PBAServiceHealthCheck implements HealthIndicator {
+
+
+    private String uri;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public PBAServiceHealthCheck(@Value("${pba.validation.health.url}") String newUri,
+                                 @Value("${pba.validation.old.health.url}") String oldUri,
+                                 @Value("${pba.validation.old.url.enabled}") boolean oldUrlEnabled) {
+        this.uri = (oldUrlEnabled ? this.uri = oldUri : newUri);
     }
+
+
+    /**
+     * Return an indication of health.
+     *
+     * @return the health for Fees service
+     */
+    @Override
+    public Health health() {
+        try {
+            List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+            MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+            converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+            messageConverters.add(converter);
+            restTemplate.setMessageConverters(messageConverters);
+            ResponseEntity<Object> response = restTemplate.getForEntity(uri, Object.class);
+            return response.getStatusCode() == (HttpStatus.OK) ? statusHealthy() : statusUnknown();
+        } catch (HttpStatusCodeException ex) {
+            log.error("Http exception occurred while doing health check on", ex);
+            return statusError(ex);
+        } catch (Exception ex) {
+            log.error("Unknown exception occurred while doing health check", ex);
+            return statusError(ex);
+        }
+    }
+
+    private Health statusError(Exception ex) {
+        return Health.down().withDetail("uri", uri).withException(ex).build();
+    }
+
+    private Health statusHealthy() {
+        return Health.up().withDetail("uri", uri).build();
+    }
+
+    private Health statusUnknown() {
+        return Health.unknown().withDetail("uri", uri).build();
+    }
+
 }
--- src/test/java/uk/gov/hmcts/reform/finrem/payments/health/PBAServiceHealthCheckTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/payments/health/PBAServiceHealthCheckTest.java
@@ -1,16 +1,91 @@
 package uk.gov.hmcts.reform.finrem.payments.health;
 
-public class PBAServiceHealthCheckTest extends AbstractServiceHealthCheckTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
-    private static final String URI = "http://pba.test/health";
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
-    @Override
-    protected String uri() {
-        return URI;
+@RunWith(MockitoJUnitRunner.class)
+public class PBAServiceHealthCheckTest {
+
+    private static final String NEW_PRD_URI = "http://localhost:9001/health";
+    private static final String OLD_PRD_URI = "http://localhost:9002/health";
+    private PBAServiceHealthCheck pbaServiceHealthCheck;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Test
+    public void shouldDoHealthCheckWithOldPrdURI() {
+        pbaServiceHealthCheck = new PBAServiceHealthCheck(NEW_PRD_URI,
+                OLD_PRD_URI, true);
+        ReflectionTestUtils.setField(pbaServiceHealthCheck, "restTemplate", restTemplate);
+        when(restTemplate.getForEntity(eq(OLD_PRD_URI), eq(Object.class)))
+                .thenReturn(ResponseEntity.ok(""));
+        assertThat(pbaServiceHealthCheck.health(), is(Health.up().withDetail("uri", OLD_PRD_URI)
+                .build()));
     }
 
-    @Override
-    protected AbstractServiceHealthCheck healthCheckInstance() {
-        return new PBAServiceHealthCheck(URI, restTemplate);
+    @Test
+    public void shouldDoHealthCheckWithNewPrdURI() {
+        pbaServiceHealthCheck = new PBAServiceHealthCheck(NEW_PRD_URI,
+                OLD_PRD_URI, false);
+        ReflectionTestUtils.setField(pbaServiceHealthCheck, "restTemplate", restTemplate);
+        when(restTemplate.getForEntity(eq(NEW_PRD_URI), eq(Object.class)))
+                .thenReturn(ResponseEntity.ok(""));
+        assertThat(pbaServiceHealthCheck.health(), is(Health.up().withDetail("uri", NEW_PRD_URI)
+                .build()));
+    }
+
+    @Test
+    public void shouldDoHealthCheckWithStatusAsDownWithHttpErrorWithNewUri() {
+        pbaServiceHealthCheck = new PBAServiceHealthCheck(NEW_PRD_URI,
+                OLD_PRD_URI, false);
+        ReflectionTestUtils.setField(pbaServiceHealthCheck, "restTemplate", restTemplate);
+        doHealthDownTest(new HttpClientErrorException(UNAUTHORIZED), NEW_PRD_URI);
+    }
+
+    @Test
+    public void shouldDoHealthCheckWithStatusAsDownWithNewUri() {
+        pbaServiceHealthCheck = new PBAServiceHealthCheck(NEW_PRD_URI,
+                OLD_PRD_URI, false);
+        ReflectionTestUtils.setField(pbaServiceHealthCheck, "restTemplate", restTemplate);
+        doHealthDownTest(new RuntimeException(), NEW_PRD_URI);
+    }
+
+    @Test
+    public void shouldDoHealthCheckWithStatusAsDownWithHttpErrorWithOldUri() {
+        pbaServiceHealthCheck = new PBAServiceHealthCheck(NEW_PRD_URI,
+                OLD_PRD_URI, true);
+        ReflectionTestUtils.setField(pbaServiceHealthCheck, "restTemplate", restTemplate);
+        doHealthDownTest(new HttpClientErrorException(UNAUTHORIZED), OLD_PRD_URI);
+    }
+
+    @Test
+    public void shouldDoHealthCheckWithStatusAsDownWithOldUri() {
+        pbaServiceHealthCheck = new PBAServiceHealthCheck(NEW_PRD_URI,
+                OLD_PRD_URI, true);
+        ReflectionTestUtils.setField(pbaServiceHealthCheck, "restTemplate", restTemplate);
+        doHealthDownTest(new RuntimeException(), OLD_PRD_URI);
+    }
+
+    private void doHealthDownTest(Exception ex, String uri) {
+
+        when(restTemplate.getForEntity(eq(uri), eq(Object.class)))
+                .thenThrow(ex);
+
+        assertThat(pbaServiceHealthCheck.health(), is(Health.down().withDetail("uri", uri).withException(ex).build()));
+
     }
 }
