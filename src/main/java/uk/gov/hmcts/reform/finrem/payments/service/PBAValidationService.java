67bd14e0ccc4cbbaaec92c981e3d471c947c4117
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2019-09-09 14:52:44 +0100

    FR_1127_new_prd_api
    - SONAR issue fix

--- src/main/java/uk/gov/hmcts/reform/finrem/payments/service/PBAValidationService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/payments/service/PBAValidationService.java
@@ -10,14 +10,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
-import uk.gov.hmcts.reform.authorisation.exceptions.InvalidTokenException;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.finrem.payments.config.PBAValidationServiceConfiguration;
 import uk.gov.hmcts.reform.finrem.payments.model.pba.validation.PBAOrganisationResponse;
 import uk.gov.hmcts.reform.finrem.payments.model.pba.validation.PBAValidationResponse;
 
 import java.net.URI;
-import java.util.Objects;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.web.util.UriComponentsBuilder.fromHttpUrl;
@@ -33,7 +31,7 @@ public class PBAValidationService {
     private final RestTemplate restTemplate;
     private final AuthTokenGenerator authTokenGenerator;
 
-    public PBAValidationResponse isPBAValid(String authToken, String pbaNumber) {
+    public PBAValidationResponse isPBAValid(final String authToken, final String pbaNumber) {
         String emailId = idamService.getUserEmailId(authToken);
         URI uri = buildUri(emailId);
         log.info("Inside isPBAValid, PRD API uri : {}, emailId : {}", uri, emailId);
@@ -42,7 +40,11 @@ public class PBAValidationService {
             if (serviceConfig.isEnableOldUrl()) {
                 request = buildRequest();
             } else {
-                request = buildRequest(authToken);
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(HttpHeaders.AUTHORIZATION, authToken);
+                headers.add("Content-Type", "application/json");
+                headers.add("ServiceAuthorization", authTokenGenerator.generate());
+                request = new HttpEntity<>(headers);
             }
             log.info("before prd call ...");
             ResponseEntity<PBAOrganisationResponse> responseEntity = restTemplate.exchange(uri, GET,
@@ -58,17 +60,8 @@ public class PBAValidationService {
         }
     }
 
-    private HttpEntity buildRequest() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");
-        return new HttpEntity<>(headers);
-    }
 
     private HttpEntity buildRequest(String authToken) {
-        if (Objects.isNull(authToken) || !authToken.startsWith("Bearer")) {
-            throw new InvalidTokenException("Invalid User Auth Token",
-                    new RuntimeException("Invalid User Auth Token" + authToken));
-        }
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.AUTHORIZATION, authToken);
         headers.add("Content-Type", "application/json");
@@ -76,6 +69,11 @@ public class PBAValidationService {
         return new HttpEntity<>(headers);
     }
 
+    private HttpEntity buildRequest() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        return new HttpEntity<>(headers);
+    }
 
     private URI buildUri(String emailId) {
         if (serviceConfig.isEnableOldUrl()) {
