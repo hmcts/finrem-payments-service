dd304fb0fcdee4913f3a9f62dd0a35b306298b76
ruban72 <ruban72@hotmail.com>
2021-02-22 11:21:40 +0000

    TA-393: Tidy up PBA tests

--- src/contractTest/java/uk/gov/hmcts/reform/finrem/payments/contract/PBAPaymentConsumerFailTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/finrem/payments/contract/PBAPaymentConsumerFailTest.java
@@ -9,18 +9,15 @@ import au.com.dius.pact.core.model.annotations.Pact;
 import au.com.dius.pact.core.model.annotations.PactFolder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.client.fluent.Executor;
-import org.json.JSONException;
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.TestPropertySource;
 import uk.gov.hmcts.reform.finrem.payments.BaseTest;
 import uk.gov.hmcts.reform.finrem.payments.model.pba.payment.PaymentRequest;
-import uk.gov.hmcts.reform.finrem.payments.model.pba.payment.PaymentResponse;
 import uk.gov.hmcts.reform.finrem.payments.service.PBAPaymentService;
 
 import java.io.IOException;
@@ -55,7 +52,7 @@ public class PBAPaymentConsumerFailTest extends BaseTest {
     }
 
     @Pact(provider = "payment_creditAccountPayment", consumer = "fr_paymentService")
-    public RequestResponsePact generatePactFragmentFail(PactDslWithProvider builder) throws JSONException, IOException {
+    public RequestResponsePact generatePactFragmentFail(PactDslWithProvider builder) throws IOException {
 
         Map<String, Object> paymentMap = new HashMap<>();
         paymentMap.put("accountNumber", "test.account");
@@ -63,32 +60,32 @@ public class PBAPaymentConsumerFailTest extends BaseTest {
         paymentMap.put("accountName", "test.account.name");
 
         return builder
-                .given("An active account has insufficient funds for a payment", paymentMap)
-                .uponReceiving("A request for a payment")
-                .path("/credit-account-payments")
-                .method("POST")
-                .headers(HttpHeaders.AUTHORIZATION, AUTH_TOKEN)
-                .body(objectMapper.writeValueAsString(getPaymentRequest(ONE_THOUSAND_FIVE_HUNDRED)))
-                .willRespondWith()
-                .status(403)
-                .body(buildPBAPaymentResponseDsl("Fail", "failed", "CA-E0001", "Insufficient funds available"))
-                .toPact();
+            .given("An active account has insufficient funds for a payment", paymentMap)
+            .uponReceiving("A request for a payment")
+            .path("/credit-account-payments")
+            .method("POST")
+            .headers(HttpHeaders.AUTHORIZATION, AUTH_TOKEN)
+            .body(objectMapper.writeValueAsString(getPaymentRequest(ONE_THOUSAND_FIVE_HUNDRED)))
+            .willRespondWith()
+            .status(403)
+            .body(buildPBAPaymentResponseDsl("Fail", "failed", "CA-E0001", "Insufficient funds available"))
+            .toPact();
     }
 
     @Test
     @PactVerification(fragment = "generatePactFragmentFail")
-    public void verifyPBAPaymentPactFail(){
+    public void verifyPBAPaymentPactFail() {
         verifyForbiddenRequest(new BigDecimal("1500.00"));
     }
 
-    private void verifyForbiddenRequest(BigDecimal ONE_THOUSAND_FIVE_HUNDRED){
-        PaymentRequest paymentRequest = getPaymentRequest(ONE_THOUSAND_FIVE_HUNDRED);
-        Exception exception = assertThrows(Exception.class, () -> {
-            PaymentResponse responseEntity = pbaPaymentService.makePayment(AUTH_TOKEN, paymentRequest);
+    private void verifyForbiddenRequest(BigDecimal amount) {
+        PaymentRequest paymentRequest = getPaymentRequest(amount);
+        assertThrows(Exception.class, () -> {
+            pbaPaymentService.makePayment(AUTH_TOKEN, paymentRequest);
         });
     }
 
-    private DslPart buildPBAPaymentResponseDsl(String status, String paymentStatus, String errorCode, String errorMessage){
+    private DslPart buildPBAPaymentResponseDsl(String status, String paymentStatus, String errorCode, String errorMessage) {
         return getDslPart(status, paymentStatus, errorCode, errorMessage);
     }
 
